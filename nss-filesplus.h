7a92a5949173db8283a1684e3ec61ed9f300b2dd
Bill Pemberton <wfp5p@worldbroken.com>
2015-02-03 14:34:32 -0500

    Add externs for all non-static functions

--- nss-filesplus.h
+++ nss-filesplus.h
@@ -38,7 +38,50 @@ struct fpent_t
 };
 
 
+/* common.c */
 extern enum nss_status internal_setent(struct fpent_t *fpinfo);
 extern enum nss_status internal_endent(struct fpent_t *fpinfo);
+
+/* group.c */
+extern enum nss_status _nss_filesplus_setgrent(void);
+extern enum nss_status _nss_filesplus_endgrent(void);
+extern enum nss_status _nss_filesplus_getgrent_r(struct group *gr, char *buffer,
+						 size_t buflen, int *errnop);
+extern enum nss_status _nss_filesplus_getgrnam_r(const char *name, struct group *gr,
+						 char *buffer, size_t buflen,
+						 int *errnop);
+extern enum nss_status _nss_filesplus_getgrgid_r(const gid_t gid, struct group *gr,
+						 char *buffer, size_t buflen,
+						 int *errnop);
+extern enum nss_status _nss_filesplus_initgroups_dyn(const char *user,
+						     gid_t gid,
+						     long int *start,
+						     long int *size,
+						     gid_t **groupsp,
+						     long int limit,
+						     int *errnop);
+
+/* passwd.c */
+extern enum nss_status _nss_filesplus_getpwuid_r(uid_t uid, struct passwd *pwd,
+						 char *buffer, size_t buflen,
+						 int *errnop);
+extern enum nss_status _nss_filesplus_getpwnam_r(const char *name,
+						 struct passwd *pwd, char *buffer,
+						 size_t buflen, int *errnop);
+extern enum nss_status _nss_filesplus_setpwent(void);
+extern enum nss_status _nss_filesplus_endpwent(void);
+extern enum nss_status _nss_filesplus_getpwent_r(struct passwd *pwd, char *buffer,
+						 size_t buflen, int *errnop);
+
+
+/* shadow.c */
+extern enum nss_status _nss_filesplus_setspent(void);
+extern enum nss_status _nss_filesplus_endspent(void);
+extern enum nss_status _nss_filesplus_getspent_r(struct spwd *spw, char *buffer,
+						 size_t buflen, int *errnop);
+extern enum nss_status _nss_filesplus_getspnam_r(const char *name, struct spwd *spw,
+						 char *buffer, size_t buflen,
+						 int *errnop);
+
 
 #endif
